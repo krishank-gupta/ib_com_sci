b787cb7b8f526063f2696f8f40660fcbbcec491a
krishank-gupta <krishankkumargupta@gmail.com>
2022-10-04 09:50:48 +0900

    update quizzes, add quiz 14

--- quizzes/quiz003/quiz003-code.py
+++ quizzes/quiz003/quiz003-code.py
@@ -2,23 +2,12 @@
 proteinInput = 'CGATAGCTAGCTGATCGATCGATCGGTCAGTCGAT'
 proteinOutput = ""
 
+inputList = ['A', 'G', 'T', 'C']
+outputList = ['T', 'C', 'A', 'G']
+
 # Loop through every character in string, replace using description from the problem
 for proteinStr in proteinInput:    
-  # If input character is A, replace with T
-  if(proteinStr == 'A'):
-    proteinOutput += 'T'
-  # If input character is G, replace with C
-  elif(proteinStr == 'G'):
-    proteinOutput += 'C'
-  # If input character is T, replace with A
-  elif(proteinStr == 'T'):
-    proteinOutput += 'A'
-  # If input character is C, replace with G
-  elif(proteinStr == 'C'):
-    proteinOutput += 'G'
-  else:
-    # Return error if input character doesn't match the description
-    proteinOutput += 'ERROR'
+  proteinOutput += outputList[inputList.index(proteinStr)]
 
 # Print Output 
 print(f"The Input was {proteinInput} and the output is {proteinOutput}")
---
+++ quizzes/quiz014/quiz014-code.py
@@ -0,0 +1,15 @@
+while True:
+  inputA = input("Please enter a number: ")
+  inputB = input("Please enter another number: ")
+  
+  try:
+    inputA = int(inputA)
+    inputB = int(inputB)
+    break
+  except ValueError:
+    print(f"You entered {inputA} and {inputB}. Please make sure both entries are numbers")
+
+def mystery(a, b):
+    return (a*a) + b
+
+print(mystery(inputA,inputB))

834ecd975f425d24ae39daaa66d39203255c8cbf
krishank-gupta <krishankkumargupta@gmail.com>
2022-11-08 09:42:19 +0900

    finish quiz022 code, add results

---
+++ unit 2/quizzes/quiz022/README.md
@@ -0,0 +1,11 @@
+# Quiz 022:
+
+# Proof
+
+A (A+B) = A
+
+# Code: (remember to scroll)
+
+# Results
+
+![quiz022-results](./quiz022-results.png)
---
+++ unit 2/quizzes/quiz022/quiz022-code.py
@@ -0,0 +1,17 @@
+import random 
+random.seed(1234)
+
+def produce(n,m,s):
+    n = int(n)
+    m = int(m)
+    s = int(s)
+    x = random.randint(0,100)
+
+    return f"| {str(x).center(10)} | {(str(round(x ** ((1/2)*((m/s)**2)),2))).center(10)} |"
+
+print(f"| {'x'.center(10)} | {'y(x)'.center(10)} |")
+print(produce(5,3,2))
+print(produce(5,3,2))
+print(produce(5,3,2))
+print(produce(5,3,2))
+print(produce(5,3,2))

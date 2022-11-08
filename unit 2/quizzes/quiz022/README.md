6c8e5b4040e10d21dfd66cf0055fcc4e0ff391cc
krishank-gupta <krishankkumargupta@gmail.com>
2022-11-08 14:27:11 +0900

    add quiz022 code to readme

--- unit 2/quizzes/quiz022/README.md
+++ unit 2/quizzes/quiz022/README.md
@@ -6,6 +6,8 @@ A (A+B) = A
 
 # Code: (remember to scroll)
 
+https://github.com/krishank-gupta/ib_com_sci/blob/834ecd975f425d24ae39daaa66d39203255c8cbf/unit%202/quizzes/quiz022/quiz022-code.py#L1-L17
+
 # Results
 
 ![quiz022-results](./quiz022-results.png)
--- unit 2/quizzes/quiz022/quiz022-code.py
+++ unit 2/quizzes/quiz022/quiz022-code.py
@@ -7,7 +7,7 @@ def produce(n,m,s):
     s = int(s)
     x = random.randint(0,100)
 
-    return f"| {str(x).center(10)} | {(str(round(x ** ((1/2)*((m/s)**2)),2))).center(10)} |"
+    return (f"| {str(x).center(10)} | {(str(round(x ** ((1/2)*((m/s)**2)),2))).center(10)} |")
 
 print(f"| {'x'.center(10)} | {'y(x)'.center(10)} |")
 print(produce(5,3,2))

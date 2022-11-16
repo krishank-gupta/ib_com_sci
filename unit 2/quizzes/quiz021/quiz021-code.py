df2b2806a75ac08706b60532e12fb437eeb23cff
krishank-gupta <krishankkumargupta@gmail.com>
2022-11-16 19:51:15 +0900

    add comments to code

--- unit 2/quizzes/quiz021/quiz021-code.py
+++ unit 2/quizzes/quiz021/quiz021-code.py
@@ -1,27 +1,45 @@
+# Main function
 def get_truth():
+
+    # Create variable with header
     initMsg = f"| A | B | C | {'AB + notB + notB C'.center(20)} |"
-    print(f"{'-'*len(initMsg)}")
-    print(initMsg)
+    
+    # Create a boundry using the header variable's length
     print(f"{'-'*len(initMsg)}")
 
+    # Print the header
+    print(initMsg)
+
+    # boundry beneath the header
+    print(f"{'-'*len(initMsg)}")
+
+   # Initialize three variables that will be the value of A B C
     a = 1
     b = 1
     c = 1
 
+    # For loop in range 0 to 2^3 (since we have three variables) with 1 increment
     for i in range(0, 8, 1):         
-     
+        
+        # Switch value of a after 4 increments
         if i % 4 == 0:
             a = not a
-            
+        
+        # Switch value of b after 2 increments
         if i%2 == 0:
             b = not b
         
+        # Switch value of c after every increment
         c = i%2
 
+        # variable for result of operation according to description of quiz
         operator = (a and b) or (not b) or ((not b) and c)
 
+        # print a,b,c and the solution
         print(f"| {int(a)} | {int(b)} | {int(c)} | {str(int(operator)).center(20)} |")
         
+    # print a border at the bottom
     print(f"{'-'*len(initMsg)}")
 
+# call the function
 get_truth()

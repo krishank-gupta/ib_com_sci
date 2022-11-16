192f98e35e70d82727a89825f3924cbdb8af737e
krishank-gupta <krishankkumargupta@gmail.com>
2022-11-16 19:33:45 +0900

    add comments to code

--- unit 2/quizzes/quiz017/quiz017-code.py
+++ unit 2/quizzes/quiz017/quiz017-code.py
@@ -1,16 +1,27 @@
+# Function that takes in words and returns 
 def averageLength(words):
-    leng = len(words)
+    # Initialize sum variable that will be used to calculate average
     sum = 0
-    for i in words:
-        i = str(i).replace(" ", "")
-        sum += len(i)
-    return sum / leng
 
+    # For every word that is inputted in the function
+    for i in words:
+
+        # Remove empty space from the word
+        i = str(i).replace(" ", "")
+        
+        # Find length of the word and add it to sum
+        sum += len(i)
+
+    # average = sum / number of words
+    return sum / len(words)
+
+# The test cases
 caseOne = ["hello", "main"]
 caseTwo = ["Peru", "France", "Nepal"]
 caseThree = ["Computer Science", "Art"]
 caseFour = ["one", "two"]
 
+# Outputs
 print(f"Words: {caseOne} Average = {averageLength(caseOne)}")
 print(f"Words: {caseTwo} Average = {averageLength(caseTwo)}")
 print(f"Words: {caseThree} Average = {averageLength(caseThree)}")

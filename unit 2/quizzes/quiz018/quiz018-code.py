335b9719e075447cddef8bf5b8dcfcffaa1ae4dc
krishank-gupta <krishankkumargupta@gmail.com>
2022-11-16 19:39:59 +0900

    add comments to code

--- unit 2/quizzes/quiz018/quiz018-code.py
+++ unit 2/quizzes/quiz018/quiz018-code.py
@@ -1,14 +1,19 @@
+# Import the math library to use the ciel function for rounding up
 import math
 
+# function that takes in the l and s
 def numberMatches(l,s):
-    return str(math.ceil((l * (100/s))/5)) + " matches"
+    # calculates output based on the question and returns from the function
+    return math.ceil((l * (100/s))/5)
 
+# Test cases
 case1 = (100,100)
 case2 = (250,110)
 case3 = (500,150)
 case4 = (12345,123)
 
-print(f"Case: {case1} Number of matches: {numberMatches(case1[0], case1[1])}")
-print(f"Case: {case2} Number of matches: {numberMatches(case2[0], case2[1])}")
-print(f"Case: {case3} Number of matches: {numberMatches(case3[0], case3[1])}")
-print(f"Case: {case4} Number of matches: {numberMatches(case4[0], case4[1])}")
+# Outputs
+print(f"Case: {case1} Number of matches: {numberMatches(case1[0], case1[1])} matches")
+print(f"Case: {case2} Number of matches: {numberMatches(case2[0], case2[1])} matches")
+print(f"Case: {case3} Number of matches: {numberMatches(case3[0], case3[1])} matches")
+print(f"Case: {case4} Number of matches: {numberMatches(case4[0], case4[1])} matches")

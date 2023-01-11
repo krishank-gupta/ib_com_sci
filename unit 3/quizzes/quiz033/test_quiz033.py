9be1cebd07f0040c378b15095e82435f0afddfad
krishank-gupta <krishankkumargupta@gmail.com>
2023-01-12 08:56:54 +0900

    update quiz033 tests

--- unit 3/quizzes/quiz033/test_quiz033.py
+++ unit 3/quizzes/quiz033/test_quiz033.py
@@ -9,12 +9,3 @@ def test_one_common_element():
 
 def test_multiple_common_elements():
   assert mystery([1, 2, 3, 4], [3, 4, 5, 6]) == [3, 4]
-
-# import inc_dec    # The code to test
-# import pytest 
-
-# def test_increment():
-#     assert inc_dec.increment(3) == 4
-
-# def test_decrement():
-#     assert inc_dec.decrement(3) == 4

607129ea91c0ae262d09c0e19d964caccf7215a1
krishank-gupta <krishankkumargupta@gmail.com>
2023-01-13 10:23:54 +0900

    create quiz 35

--- unit 3/quizzes/quiz034/quiz034.py
+++ unit 3/quizzes/quiz034/quiz034.py
@@ -1,4 +1,5 @@
 roman_dict = {
+    100: 'C',
     50: 'L',
     10: 'X',
     5: 'V',
@@ -9,21 +10,35 @@ def to_roman(num):
     num = int(num)
     output = ''
 
-    i = roman_dict
     while num > 0:
-        
-        if num - i >= 0:
-            output += roman_dict[i]
-            num -= i
-        i += 1
-    
+        for i in roman_dict:
+            if num - i >= 0:
+                output += roman_dict[i]
+                num -= i
+        # if num - 100 >= 0:
+        #     output += roman_dict[100]
+        #     num -= 100
+        # elif num - 50 >= 0:
+        #     output += roman_dict[50]
+        #     num -= 50
+        # elif num - 10 >= 0:
+        #     output += roman_dict[10]
+        #     num -= 10
+        # elif num - 5 >= 0:
+        #     output += roman_dict[5]
+        #     num -= 5
+        # elif num - 1 >= 0:
+        #     output += roman_dict[1]
+        #     num -= 1
+
     if num > 100:
         raise ValueError("Error. Maximum Value 100")
-
-    return output
-
+    else:
+        return output
 
 print(to_roman(1))
+print(to_roman(5))
 print(to_roman(10))
-print(to_roman(60))
-print(to_roman(70))
+print(to_roman(20))
+print(to_roman(80))
+print(to_roman(90))

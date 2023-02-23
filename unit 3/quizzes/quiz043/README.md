5ff438fa8e4e7a7701dec8d3aebaafe5cb5615c1
krishank-gupta <krishankkumargupta@gmail.com>
2023-02-23 09:02:20 +0900

    upload files

---
+++ unit 3/quizzes/quiz043/README.md
@@ -0,0 +1,15 @@
+# Quiz 42: 
+
+In this quiz we learned about databases.
+
+# Link to the python file: (remember to scroll)
+
+#L1-L16
+
+# Link to the kivy file: (remember to scroll)
+
+#L1-L32
+
+# Results 
+
+![quiz43results](./results.png)
---
+++ unit 3/quizzes/quiz043/quiz043.session.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE Movies(
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     year INTEGER NOT NULL,
+--     producer VARCHAR(255) NOT NULL,
+--     director VARCHAR(255) NOT NULL,
+--     budget INTEGER NOT NULL,
+--     category VARCHAR(255) NOT NULL
+-- );
+
+-- INSERT INTO Movies(name, year, producer, director, budget, category) values ("Harry Potter and the Prisoner of Azkaban", 2004, "Chris Columbus", "Alfonso Cuarón", 130000000, "Adventure");
+
+-- INSERT INTO Movies(name, year, producer, director, budget, category) values ("Passengers", 2016, "Start Motion Pictures", "Morten Tyldum", 110000000, "Romance");
+
+select * from Movies;
